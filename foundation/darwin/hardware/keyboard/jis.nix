fed6b5fadc29952676eeb70cb5242208cf6c6799
i10416 <ito.yo16uh90616@gmail.com>
2025-05-09 03:46:35 +0900

    setup: remap capslock to fn
    Remap capslock key to fn in accordance with US keyboard.
    This allows developers to control window position and size by one hand.

--- foundation/darwin/hardware/keyboard/jis.nix
+++ foundation/darwin/hardware/keyboard/jis.nix
@@ -2,12 +2,13 @@
   {
     # 0x700000039:caps lock
     HIDKeyboardModifierMappingSrc = 30064771129;
-    # Keycode for ABC(英数) is keyboard_lang2: https://github.com/pqrs-org/Karabiner-Elements/blob/294d1376053d7c2af9e7344e7366394b06ad001f/src/share/types/momentary_switch_event_details/key_code.hpp#L16ß
-    # and keyboard_lang2 is 145: https://github.com/ferfebles/atreus-2leds1speaker-ragel/blob/master/usb_keyboard.h
-    HIDKeyboardModifierMappingDst = 30064771217;
+    # 0xFF00000003: fn
+    HIDKeyboardModifierMappingDst = 1095216660483;
   }
   {
     # 0x700000091:ABC
+    # Keycode for ABC(英数) is keyboard_lang2: https://github.com/pqrs-org/Karabiner-Elements/blob/294d1376053d7c2af9e7344e7366394b06ad001f/src/share/types/momentary_switch_event_details/key_code.hpp#L16ß
+    # and keyboard_lang2 is 145: https://github.com/ferfebles/atreus-2leds1speaker-ragel/blob/master/usb_keyboard.h
     HIDKeyboardModifierMappingSrc = 30064771217;
     # 0x7000000E3:CMD
     HIDKeyboardModifierMappingDst = 30064771299;
